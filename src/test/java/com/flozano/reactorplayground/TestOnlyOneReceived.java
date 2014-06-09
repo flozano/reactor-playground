822a755d0cc827f5fd8c891e54efda1e0217633e
Francisco A. Lozano <flozano@gmail.com>
2014-06-09 16:04:40 +0200

    Assert for repeated items, 1.0.1.RELEASE

--- src/test/java/com/flozano/reactorplayground/TestOnlyOneReceived.java
+++ src/test/java/com/flozano/reactorplayground/TestOnlyOneReceived.java
@@ -2,8 +2,11 @@ package com.flozano.reactorplayground;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.CountDownLatch;
 
 import org.junit.Before;
@@ -18,7 +21,7 @@ import reactor.function.Consumer;
 
 public class TestOnlyOneReceived {
 
-	static final int N = 100;
+	static final int N = 10;
 	Reactor r;
 
 	@Before
@@ -41,20 +44,24 @@ public class TestOnlyOneReceived {
 			r.notify("test", Event.wrap(i));
 		}
 		c.latch.await();
+		assertEquals(0, c.repeatedItems.size());
 		assertEquals(expected, c.items);
 	}
 
 	public static class TestConsumer implements Consumer<Event<Integer>> {
-		final Set<Integer> items = new TreeSet<Integer>();
+		final Set<Integer> items = new CopyOnWriteArraySet<Integer>();
 		final CountDownLatch latch = new CountDownLatch(N);
+		final List<Integer> repeatedItems = new CopyOnWriteArrayList<Integer>();
 
 		public void accept(Event<Integer> arg0) {
 			if (!items.add(arg0.getData())) {
-				throw new IllegalArgumentException("Already added: %"
-						+ arg0.getData());
+				System.err.println("Failed to add " + arg0.getData()
+						+ " - repeated value!!!!");
+				repeatedItems.add(arg0.getData());
 			}
-			;
 			latch.countDown();
+			System.err.println(arg0.getData() + " received, "
+					+ latch.getCount() + " to go");
 		}
 	}
 }
